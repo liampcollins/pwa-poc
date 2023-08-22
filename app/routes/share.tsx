9fcb619504a3043228a368d4a436578485fc64d4
liampcollins <liampcollins@gmail.com>
2023-08-22 08:44:57 +0100

    more

--- app/routes/share.tsx
+++ app/routes/share.tsx
@@ -4,5 +4,13 @@ import React from "react";
 export default function ShareHandler() {
   const params = useParams();
   console.log("🚀 ~ ShareHandler ~ params:", params);
-  return <div>ShareHandler</div>;
+
+  // some nice UI that displays the url in the params object
+  return (
+    <div>
+      <h1>Share</h1>
+      <p>URL: {params.url}</p>
+      <p>{JSON.stringify(params)}</p>
+    </div>
+  );
 }
