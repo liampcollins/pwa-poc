3c3d65b2bedca9a4da101df5bc1c4557273a71a1
liampcollins <liampcollins@gmail.com>
2023-08-22 08:56:23 +0100

    more

--- app/routes/share.tsx
+++ app/routes/share.tsx
@@ -1,19 +1,19 @@
-import { useLocation, useParams } from "@remix-run/react";
+import { useLocation, useParams, useSearchParams } from "@remix-run/react";
 import React from "react";
 
 export default function ShareHandler() {
-  const params = useParams();
+  const params = useSearchParams();
   const location = useLocation();
-  console.log("🚀 ~ ShareHandler ~ params:", params);
+  console.log("🚀 ~ ShareHandler ~ params:", params[0]);
 
   // some nice UI that displays the url in the params object
   return (
     <div>
       <h1>Share</h1>
-      <p>URL: {params.url}</p>
-      <p>{JSON.stringify(params)}</p>
-      <p>{JSON.stringify(location)}</p>
-      <p>{location.search}</p>
+      <p>URL: {location.search}</p>
+      {/* <p>{JSON.stringify(params)}</p>
+      <p>{JSON.stringify(location)}</p> */}
+      {/* <p>{location.search}</p> */}
     </div>
   );
 }
